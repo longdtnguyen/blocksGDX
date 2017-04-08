86e109fa78bdb6a92062717c2571c95c2dabcb51
longdtnguyen <l.nguyen2606@gmail.com>
2017-04-08 19:26:56 -0400

    Initial commit

---
+++ android/src/com/longdtnguyen/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.longdtnguyen.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.longdtnguyen.game.GamePanel;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GamePanel(), config);
+	}
+}
